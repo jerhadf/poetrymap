37614075a9eeee9a7fd9011fc3a85019e6a3371d
jerhadf <jhadfield129@gmail.com>
2023-12-31 17:52:18 -0700

    Map complete with pin adding

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,71 +1,105 @@
 'use client'
 
-import L from 'leaflet'
-import 'leaflet/dist/leaflet.css'
-import { useState } from 'react'
-import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
-import MarkerIcon from '../node_modules/leaflet/dist/images/marker-icon.png'
-import MarkerShadow from '../node_modules/leaflet/dist/images/marker-shadow.png'
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogTitle from '@mui/material/DialogTitle';
+import TextField from '@mui/material/TextField';
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+import { useState } from 'react';
+import { MapContainer, Marker, TileLayer, useMapEvents } from 'react-leaflet';
+import MarkerIcon from '../node_modules/leaflet/dist/images/marker-icon.png';
+import MarkerShadow from '../node_modules/leaflet/dist/images/marker-shadow.png';
 
 const Map = () => {
+  const [markers, setMarkers] = useState<Array<{position: [number, number], link: string, title: string}>>([]);
+  const [tempPin, setTempPin] = useState<[number, number] | null>(null);
+  const [open, setOpen] = useState(false);
+  const [link, setLink] = useState('');
+  const [title, setTitle] = useState('');
+  // center of the US by default
+  const defaultLocation: [number, number] = [39.50, -98.35];
 
-  const [coord, setCoord] = useState<[number, number]>([51.505, -0.09]);
-
-    const SearchLocation = () => {
-        return (
-            <div className="search-location">
-                <input type="text" placeholder="Search Location" />
-            </div>
-        )
+  const addPin = () => {
+    if (tempPin) {
+      setMarkers([...markers, {position: tempPin, link, title}]);
+      setTempPin(null);
+      setOpen(false);
     }
+  };
 
-    const GetMyLocation = () => {
-        const getMyLocation = () => {
-            if (navigator.geolocation) {
-                navigator.geolocation.getCurrentPosition((position) => {
-                    setCoord([position.coords.latitude, position.coords.longitude])
-                })
-            } else {
-                console.log("Geolocation is not supported by this browser.")
+  const MapEvents = () => {
+    useMapEvents({
+      click: (e) => {
+        setTempPin([e.latlng.lat, e.latlng.lng]);
+        setOpen(true);
+      },
+    });
+    return null;
+  };
+
+  return (
+    <div>
+      <MapContainer style={{ height: '100vh', width: '100vw' }} center={defaultLocation} zoom={4}>
+        <MapEvents />
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {markers.map((marker, idx) => (
+          <Marker
+            key={idx}
+            position={marker.position}
+            icon={
+              new L.Icon({
+                iconUrl: MarkerIcon.src,
+                iconRetinaUrl: MarkerIcon.src,
+                iconSize: [25, 41],
+                iconAnchor: [12.5, 41],
+                popupAnchor: [0, -41],
+                shadowUrl: MarkerShadow.src,
+                shadowSize: [41, 41],
+              })
             }
-        }
-
-        return (
-            <div className="get-my-location">
-                <button onClick={getMyLocation}>Get My Location</button>
-            </div>
-        )
-    }
-
-    return (
-        <div>
-            <MapContainer style={{
-                height: '100vh',
-                width: '100vw'
-            }} center={coord} zoom={13} scrollWheelZoom={false}>
-                <TileLayer
-                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                />
-
-                <Marker icon={
-                    new L.Icon({
-                        iconUrl: MarkerIcon.src,
-                        iconRetinaUrl: MarkerIcon.src,
-                        iconSize: [25, 41],
-                        iconAnchor: [12.5, 41],
-                        popupAnchor: [0, -41],
-                        shadowUrl: MarkerShadow.src,
-                        shadowSize: [41, 41],
-                    })
-                } position={[51.505, -0.09]}>
-                     <Popup>
-                        A pretty CSS3 popup. <br /> Easily customizable.
-                    </Popup>
-                </Marker>
-            </MapContainer>
-        </div>
-    )
+          />
+        ))}
+        </MapContainer>
+        <Dialog open={open} onClose={() => setOpen(false)}>
+        <DialogTitle>Add Pin</DialogTitle>
+        <DialogContent>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="link"
+            label="Link"
+            type="text"
+            fullWidth
+            value={link}
+            onChange={(e) => setLink(e.target.value)}
+          />
+          <TextField
+            margin="dense"
+            id="title"
+            label="Title"
+            type="text"
+            fullWidth
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={() => setOpen(false)} color="primary">
+            Cancel
+          </Button>
+          <Button onClick={addPin} color="primary">
+            Add
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  )
 }
 
 export default Map
