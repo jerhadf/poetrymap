23586ef1c61c75d368c4b3ff085c92ec27eac6d3
jerhadf <jhadfield129@gmail.com>
2023-12-31 17:08:16 -0700

    Working basic map

---
+++ components/Map.tsx
@@ -0,0 +1,71 @@
+'use client'
+
+import L from 'leaflet'
+import 'leaflet/dist/leaflet.css'
+import { useState } from 'react'
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
+import MarkerIcon from '../node_modules/leaflet/dist/images/marker-icon.png'
+import MarkerShadow from '../node_modules/leaflet/dist/images/marker-shadow.png'
+
+const Map = () => {
+
+  const [coord, setCoord] = useState<[number, number]>([51.505, -0.09]);
+
+    const SearchLocation = () => {
+        return (
+            <div className="search-location">
+                <input type="text" placeholder="Search Location" />
+            </div>
+        )
+    }
+
+    const GetMyLocation = () => {
+        const getMyLocation = () => {
+            if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition((position) => {
+                    setCoord([position.coords.latitude, position.coords.longitude])
+                })
+            } else {
+                console.log("Geolocation is not supported by this browser.")
+            }
+        }
+
+        return (
+            <div className="get-my-location">
+                <button onClick={getMyLocation}>Get My Location</button>
+            </div>
+        )
+    }
+
+    return (
+        <div>
+            <MapContainer style={{
+                height: '100vh',
+                width: '100vw'
+            }} center={coord} zoom={13} scrollWheelZoom={false}>
+                <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+
+                <Marker icon={
+                    new L.Icon({
+                        iconUrl: MarkerIcon.src,
+                        iconRetinaUrl: MarkerIcon.src,
+                        iconSize: [25, 41],
+                        iconAnchor: [12.5, 41],
+                        popupAnchor: [0, -41],
+                        shadowUrl: MarkerShadow.src,
+                        shadowSize: [41, 41],
+                    })
+                } position={[51.505, -0.09]}>
+                     <Popup>
+                        A pretty CSS3 popup. <br /> Easily customizable.
+                    </Popup>
+                </Marker>
+            </MapContainer>
+        </div>
+    )
+}
+
+export default Map
