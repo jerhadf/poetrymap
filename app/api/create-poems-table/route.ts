f763c0f0d462723bfecbee7e2119a7073ae327cf
jerhadf <jhadfield129@gmail.com>
2023-12-31 19:44:19 -0700

    Added database

---
+++ app/api/create-poems-table/route.ts
@@ -0,0 +1,22 @@
+import { sql } from '@vercel/postgres';
+import csv from 'csv-parser';
+import fs from 'fs';
+import { NextResponse } from 'next/server';
+
+export async function GET(request: Request) {
+  try {
+    const result =
+    // create the table for the poems if it doesn't exist
+    await sql`CREATE TABLE Poems (location varchar(255), title varchar(255), url varchar(255));`;
+    // Read the CSV file and insert the data into the table
+    fs.createReadStream('/scripts/poems_table.csv')
+      .pipe(csv())
+      .on('data', async (row) => {
+        const { location, title, url } = row;
+        await sql`INSERT INTO Poems (location, title, url) VALUES (${location}, ${title}, ${url});`;
+      });
+    return NextResponse.json({ result }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ error }, { status: 500 });
+  }
+}
