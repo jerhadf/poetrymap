34f4efac62a811f83452861c019a6c6987dee7cd
jerhadf <jhadfield129@gmail.com>
2023-12-31 21:59:05 -0700

    Fixed poem loading, represent poem table as JSON

--- lib/loadPoems.ts
+++ lib/loadPoems.ts
@@ -1,6 +1,5 @@
 import { addPoem } from '@/lib/dbOperations';
-import csv from 'csv-parser';
-import fs from 'fs';
+import poems from '@/scripts/poems_table.json';
 
 type Poem = {
     location: string;
@@ -9,24 +8,8 @@ type Poem = {
 };
 
 export async function loadPoems() {
-  return new Promise((resolve, reject) => {
-    fs.createReadStream('/scripts/poems_table.csv')
-      .on('error', (error) => {
-        console.error('Error reading CSV file:', error);
-        reject(error);
-      })
-      .pipe(csv())
-      .on('data', async (row) => {
-        const { location, title, url } = row;
-        const poem: Poem = {
-          location,
-          title,
-          url,
-        };
-
-        // Add the poem to the database
-        await addPoem(poem);
-      })
-      .on('end', resolve);
-  });
-}
+    for (const poem of poems) {
+      // Add the poem to the database
+      await addPoem(poem as Poem);
+    }
+  }
